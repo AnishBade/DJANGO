96771beb8c8ff4599a9f7d5e19d536e97b3c8d90
AnishBade <badeanish6045@gmail.com>
2020-12-25 12:02:36 +0545

    New

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     # post views
     path('', views.post_list, name='post_list'),
     path('<int:year>/<int:month>/<int:day>/<slug:post>/',views.post_detail,name='post_detail'),
-]
+]   
